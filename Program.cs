27399f53f87e33e1f739173903bd71a3a48049d5
Глеб <@HOME-PC>
2024-02-06 15:28:06 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Задание 1
+            //for (int x = 0; x <= 10; x++)
+            //{
+            //    Console.WriteLine("A"+" ");
+            //}
+            //Console.ReadKey();
+
+            //Задание 2
+            //for(int x =50;x>=40;x--)
+            //{
+            //    Console.WriteLine(x);
+            //}
+            //Console.ReadKey();
+            ////Задание 3
+            //for (int x = 6; x <= 36;x+=6)
+            //{
+            //    if (x != 18) Console.WriteLine($"{x} +  \t");
+            //}
+            //Console.ReadKey();
+            //Задание 4
+            //int sum = 0;
+            //int count = 0;
+            //for(int x = 10;x>=99;x++)
+            //{
+            //    if(x%11==0)
+            //    {
+            //        Console.WriteLine(x);
+            //        count++;
+            //        sum += x;
+            //    }
+                
+            //}
+            //Console.WriteLine($"количество{count}, сумма{sum}");
+            //Console.ReadKey();
+        }
+
+    }
+}
